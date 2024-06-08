796654f3e6c21711b80e514519601b3baefdd460
bluewly7 <bluewly7@naver.com>
2024-06-08 18:48:36 +0900

    update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,23 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+const admin = require("firebase-admin");
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCQKRM4YAygiOaZv-18qL9M8sU-MBTrldQ",
-  authDomain: "fir-wheats-8c507.firebaseapp.com",
-  projectId: "fir-wheats-8c507",
-  storageBucket: "fir-wheats-8c507.appspot.com",
-  messagingSenderId: "939205124826",
-  appId: "1:939205124826:web:5eb441cbe1010a63237be3",
-  measurementId: "G-HQBWL5GPQ8",
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const auth = getAuth(app);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+});
 
-export { app, db, auth, analytics };
+const db = admin.firestore();
+
+module.exports = { admin, db };
